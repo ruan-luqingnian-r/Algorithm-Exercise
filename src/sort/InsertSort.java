80ae887f3e40ba23549e8a80bef57f62a4cc1eba
ruan-luqingnian-r <15991875018@163.com>
2021-06-13 15:15:18 +0800

    InsertSort.java Signed-off-by: ruan-luqingnian-r <15991875018@163.com>

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * @PackgeName: sort
  * @ClassName: InsertSort
@@ -12,6 +14,8 @@ package sort;
 public class InsertSort {
     public static void main(String[] args) {
         int[] arr = {2,32,13,12,412,123,12,41};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     /**
@@ -22,11 +26,19 @@ public class InsertSort {
      * 2.其他数字为无序数组的值
      * 3.将无需数组的值在有序数组中找到待插入位置
      * 4.将该数据插入
+     * 时间复杂度O(n^2)
      */
     public static int[] sort(int[] arr){
         for (int i = 1; i < arr.length; i++) {
             int value = arr[i];//从第二个数字作为待排数字
             int index = i - 1;//待排数字前数字的下标
+            while (index >= 0 && arr[index] > value){
+                //之前的数字大于待排数字
+                //将待排数据加入数组前
+                arr[index + 1] = arr[index];
+                index--;//??????
+            }
+            arr[index + 1] = value;//??????
         }
         return arr;
     }
