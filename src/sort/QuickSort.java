e8a91e8570b539ce3a2a57260a27ba61c660b900
ruan-luqingnian-r <15991875018@163.com>
2021-06-21 07:40:39 +0800

    12

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -7,7 +7,35 @@ package sort;
  * Date: 2021/6/21 7:30
  * project name: Algorithm-Exercise
  * @Version: 0.0.1
- * @Description: 
+ * @Description: 快速排序
  */
 public class QuickSort {
+
+    public static void main(String[] args) {
+
+    }
+    public static int[] sort(int[] arr,int left,int right){
+        int l = left;//左索引
+        int r = right;//右索引
+        int pivot = arr[(left + right) / 2];//中轴
+        int temp = 0;
+
+        while (r > l){
+            if (arr[l] < pivot){
+                l++;
+            }
+            if (arr[r] > pivot){
+                r--;
+            }
+            if (l > r){
+                break;
+            }
+
+            //开始交换
+            temp = arr[l];
+            arr[r] = arr[l];
+        }
+
+        return arr;
+    }
 }
