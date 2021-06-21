fbc6b5236216dd8a3693da627edbb1ff3603f050
ruan-luqingnian-r <15991875018@163.com>
2021-06-21 10:57:07 +0800

    QuickSort.java

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,6 +1,8 @@
 package sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @PackgeName: sort
@@ -14,9 +16,23 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] arr = {12312,312,312,312,124,123,12,31};
-        int[] sort = sort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(sort));
+//        int[] arr = {12312,312,312,312,124,123,12,31};
+//        int[] sort = sort(arr, 0, arr.length - 1);
+//        System.out.println(Arrays.toString(sort));
+        //性能测试
+        int[] arr = new int[80000000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 800000);//[0,8000000)的随机数
+        }
+
+        Date date1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S");
+        String date1Ser = simpleDateFormat.format(date1);
+        System.out.println("排序前的时间是:"+date1Ser);
+        sort(arr,0,arr.length - 1);
+        Date date2 = new Date();
+        String date2Ser = simpleDateFormat.format(date2);
+        System.out.println("排序后的时间是:"+date2Ser);
     }
     public static int[] sort(int[] arr,int left,int right){
         int l = left;//左索引
@@ -46,6 +62,7 @@ public class QuickSort {
                 l++;
             }
         }
+        //防止溢出
         if (l == r){
             l = l + 1;
             r = r - 1;
