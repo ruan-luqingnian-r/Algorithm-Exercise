9132818998f57248e5d335f39e09ffe9df4b6e72
ruan-luqingnian-r <15991875018@163.com>
2021-07-09 15:46:49 +0800

    SelectSort.java

--- src/ruan/SelectSort.java
+++ src/ruan/SelectSort.java
@@ -9,16 +9,28 @@ import java.util.Arrays;
  */
 public class SelectSort {
     public static void main(String[] args) {
-        int[] arr = {12,321,312,42,5,12343,5,23,124};
+        int[] arr = {3,2,4,6,1,8,6,9};
         System.out.println("排序前:" + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("排序后:" + Arrays.toString(arr));
     }
-    public void sort(int[] arr){
+    public static void sort(int[] arr){
         //假设最小值的位置是第一个
-        int minPos = 0;
-
-
-
-
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int minPos = i;
+            //遍历数组找到真正的最小值
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            //交换两个数字
+            temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+            System.out.println("第" + (i+1) + "次排序:" + Arrays.toString(arr));
+        }
     }
 
 }
