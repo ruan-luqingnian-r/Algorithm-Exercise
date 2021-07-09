793d0cc237581178620dfbd8acf732ea5931dbf2
ruan-luqingnian-r <15991875018@163.com>
2021-07-09 15:33:27 +0800

    SelectSort.java

---
+++ src/ruan/SelectSort.java
@@ -0,0 +1,24 @@
+package ruan;
+
+import java.util.Arrays;
+
+/**
+ * @Author: ruan
+ * Date: 2021/7/9 15:28
+ * @Description: 选择排序
+ */
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = {12,321,312,42,5,12343,5,23,124};
+        System.out.println("排序前:" + Arrays.toString(arr));
+    }
+    public void sort(int[] arr){
+        //假设最小值的位置是第一个
+        int minPos = 0;
+
+
+
+
+    }
+
+}
