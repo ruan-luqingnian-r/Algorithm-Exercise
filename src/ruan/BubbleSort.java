8466be01b2ed84d5745e77176d665bce377105d3
ruan-luqingnian-r <15991875018@163.com>
2021-07-10 18:02:47 +0800

    BubbleSort.java

---
+++ src/ruan/BubbleSort.java
@@ -0,0 +1,25 @@
+package ruan;
+
+/**
+ * @Author: ruan
+ * Date: 2021/7/10 17:21
+ * @Description:
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {123,23,12,42,1234,2,12,34,212,3};
+
+    }
+
+    public static void bubbleSort(int[] arr){
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length; j++) {
+
+            }
+        }
+    }
+
+
+
+}
