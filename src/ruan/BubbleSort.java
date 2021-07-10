9634daae225bfaff199355f6b2cc2170bab5c30f
ruan-luqingnian-r <15991875018@163.com>
2021-07-10 18:45:00 +0800

    BubbleSort.java

--- src/ruan/BubbleSort.java
+++ src/ruan/BubbleSort.java
@@ -1,5 +1,7 @@
 package ruan;
 
+import java.util.Arrays;
+
 /**
  * @Author: ruan
  * Date: 2021/7/10 17:21
@@ -8,16 +10,26 @@ package ruan;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {123,23,12,42,1234,2,12,34,212,3};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
 
     }
 
     public static void bubbleSort(int[] arr){
         int temp = 0;
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length; j++) {
-
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                //比较当前数字和下一个数字的大小
+                if (arr[j] > arr[j + 1]){
+                    //两数交换位置
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
+
+
     }
 
 
