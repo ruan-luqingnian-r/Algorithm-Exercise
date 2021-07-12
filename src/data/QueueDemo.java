4b9b8c4433fcd052d4f187278b2094ce3b38a599
ruan-luqingnian-r <15991875018@163.com>
2021-07-12 17:55:58 +0800

    QueueDemo.java

--- src/data/QueueDemo.java
+++ src/data/QueueDemo.java
@@ -1,6 +1,7 @@
 package data;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @Author: ruan
@@ -8,6 +9,45 @@ import java.util.Arrays;
  * @Description: 队列
  */
 public class QueueDemo {
+    public static void main(String[] args) {
+        //操作菜单
+        boolean flag = true;
+        ArrayQueue arrayQueue = new ArrayQueue(6);
+        Scanner scanner = new Scanner(System.in);
+        char key;
+
+        while (flag){
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("p(pop): 从队列取出数据");
+            System.out.println("g(get): 查看队列头的数据");
+            System.out.print("请操作：");
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's' : arrayQueue.showQueue();
+                    break;
+                case 'e' :
+                    System.out.println("==退出程序==");
+                    flag = false;
+                    break;
+                case 'a' :
+                    System.out.print("请输入:");
+                    int value = scanner.nextInt();
+                    arrayQueue.addQueue(value);
+                    break;
+                case 'p' : int i = arrayQueue.popQueue();
+                    System.out.println("===取出的数据是===");
+                    System.out.println(i);
+                    break;
+                case 'g' : int j = arrayQueue.getQueue();
+                    System.out.println("===对头的数据是===");
+                    System.out.println(j);
+                    break;
+            }
+        }
+
+    }
 
 
 
@@ -70,6 +110,6 @@ class ArrayQueue{
         if (isNull()){
             System.out.println("队列为空，无数据");
         }
-        return arr[front++];
+        return arr[front + 1];
     }
 }
