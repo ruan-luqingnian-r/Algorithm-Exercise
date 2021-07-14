d861e45bf96203b9524079249a16655928c9b3f6
ruan-luqingnian-r <15991875018@163.com>
2021-07-14 15:38:17 +0800

    123

---
+++ src/sort/Sort.java
@@ -0,0 +1,17 @@
+package sort;
+
+/**
+ * @Author: ruan
+ * Date: 2021/7/13 17:26
+ * @Description: 所有排序算法
+ */
+public class Sort {
+    public static void main(String[] args) {
+        int[] arr = {12,312,421,23,233,2,234,1,2,4,23,13,412,,41,3,2,123,124,121,412,1,123};
+    }
+
+    //冒泡
+    public static void sort(int[] arr){
+
+    }
+}
