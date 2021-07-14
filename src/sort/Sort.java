3c5b4399f69e1f0919528bec871bbbc3374a80b7
ruan-luqingnian-r <15991875018@163.com>
2021-07-14 23:36:21 +0800

    InsertSort.java

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * @Author: ruan
  * Date: 2021/7/13 17:26
@@ -7,11 +9,26 @@ package sort;
  */
 public class Sort {
     public static void main(String[] args) {
-        int[] arr = {12,312,421,23,233,2,234,1,2,4,23,13,412,,41,3,2,123,124,121,412,1,123};
+        int[] arr = {12,312,421,23,233,2,234,1,2,4,23,13,412,41,3,2,123,124,121,412,1,123};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     //冒泡
     public static void sort(int[] arr){
+        int temp = 0;//辅助变量用于
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]){
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+
 
 
     }
