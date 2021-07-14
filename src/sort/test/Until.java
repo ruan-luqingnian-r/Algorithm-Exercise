b7acf7c10eac0745253941086222e10cccf85136
ruan-luqingnian-r <15991875018@163.com>
2021-07-14 23:31:17 +0800

    InsertSort.java

--- src/sort/test/Until.java
+++ src/sort/test/Until.java
@@ -5,5 +5,9 @@ package sort.test;
  * Date: 2021/7/14 23:28
  * @Description: 抽取两数交换方法
  */
-public class Exchange {
+public class Until {
+
+    public static void exchange(int i,int j){
+
+    }
 }
