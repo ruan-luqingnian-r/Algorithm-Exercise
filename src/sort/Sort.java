2be767bc414d1cd1b23d4723bcc294e7072f9de9
ruan-luqingnian-r <15991875018@163.com>
2021-07-16 21:08:52 +0800

    InsertSort.java

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -10,12 +10,12 @@ import java.util.Arrays;
 public class Sort {
     public static void main(String[] args) {
         int[] arr = {12,312,421,23,233,2,234,1,2,4,23,13,412,41,3,2,123,124,121,412,1,123};
-        sort(arr);
+
         System.out.println(Arrays.toString(arr));
     }
 
     //冒泡
-    public static void sort(int[] arr){
+    public static void bubbleSort(int[] arr){
         int temp = 0;//辅助变量用于
 
         for (int i = 0; i < arr.length; i++) {
@@ -28,5 +28,9 @@ public class Sort {
 
             }
         }
+    }
+    //选择排序
+    public static void selectSort(int[] arr){
+
     }
 }
