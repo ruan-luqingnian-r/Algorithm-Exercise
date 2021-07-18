21672fe2da89723209f2c787dce53c797635a438
ruan-luqingnian-r <15991875018@163.com>
2021-07-18 12:26:45 +0800

    Recv1.java

---
+++ src/src/main/java/direct/Recv1.java
@@ -0,0 +1,42 @@
+package direct;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+/**
+ * @Author: ruan
+ * Date: 2021/7/16 16:11
+ * @Description: 工作队列轮询模式
+ */
+public class Recv1 {
+    private final static String EXCHANGE_NAME = "exchange_direct";
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("8.140.16.6");
+        factory.setUsername("admin");
+        factory.setPassword("password");
+        factory.setVirtualHost("/dev");
+        factory.setPort(5672);
+        Connection connection = factory.newConnection();//获取链接
+        Channel channel = connection.createChannel();//获取信道
+        //绑定交换机
+        channel.exchangeDeclare(EXCHANGE_NAME,BuiltinExchangeType.DIRECT);
+        //获取队列
+        String queueName = channel.queueDeclare().getQueue();
+        //绑定交换机和队列
+        channel.queueBind(queueName,EXCHANGE_NAME,"error");
+        channel.queueBind(queueName,EXCHANGE_NAME,"info");
+        channel.queueBind(queueName,EXCHANGE_NAME,"debug");
+        //消费数据
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+
+                System.out.println(new String(body,"UTF-8"));
+                channel.basicAck(envelope.getDeliveryTag(),false);
+            }
+        };
+        channel.basicConsume(queueName,false,consumer);
+    }
+}
