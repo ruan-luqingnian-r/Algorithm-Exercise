5eee503e1f2f15b358fe65006b80b4f926c528e7
ruan-luqingnian-r <15991875018@163.com>
2021-08-03 21:58:40 +0800

    BubbleSort.java.java

--- src/test/BubbleSort.java
+++ src/test/BubbleSort.java
@@ -7,7 +7,18 @@ package test;
  * 时间复杂度：O(n^2)
  * 算法思想：
  *  从第一个数字开始逐一与其他的数据进行比较
- *
+ *  若arr[i] > arr[i+1] 交换两个数字
  */
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2,3,2,4,3,5,1,5,6,2,6,7,545,465,45533,757,6,867,8,5,566,36};
+    }
+
+    public void sort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+            }
+        }
+    }
 }
