ff07b2bc8bd237ce0dd711a41389ceb171087f5a
ruan-luqingnian-r <15991875018@163.com>
2021-08-04 21:15:28 +0800

    LinearSearch.java

--- src/find/LinearSearch.java
+++ src/find/LinearSearch.java
@@ -7,6 +7,18 @@ package find;
  */
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {};
+        int[] arr = {1, 2, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 7, 8, 36, 465, 545, 566, 757, 867, 45533};
+        System.out.println(find(arr,46));
     }
+
+    public static int find(int[] arr,int target){
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target){
+               return i;
+            }
+        }
+        return -1;
+    }
+
 }
