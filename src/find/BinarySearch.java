872b0c132a517e56f88eeaad98f7328286ee9144
ruan-luqingnian-r <15991875018@163.com>
2021-08-07 19:44:53 +0800

    BinarySearch.java

--- src/find/BinarySearch.java
+++ src/find/BinarySearch.java
@@ -1,5 +1,8 @@
 package find;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: ruan
  * Date: 2021/8/4 21:18
@@ -22,6 +25,7 @@ public class BinarySearch {
      * @param right 右索引
      * @return 数组的下标
      */
+    //初级版本
     public static int find(int[] arr,int target,int left,int right){
         if (left > right){
             return -1;
@@ -37,5 +41,34 @@ public class BinarySearch {
             return find(arr, target, left, mid - 1);
         }
         return mid;
+    }
+    //高级版
+    public static List<Integer> binarySearch(int[] arr,int target,int left,int right){
+        //如果left > right说明遍历结束未找到目标值
+        if (left > right){
+            return new ArrayList<Integer>();
+        }
+        //中值
+        int mid = (right + left) / 2;
+        int midVel = arr[mid];
+
+        if (midVel > target){
+            //向右递归
+            return binarySearch(arr, target, mid + 1, right);
+        }else if (midVel < target){
+            //向左
+            return binarySearch(arr, target, left, mid - 1);
+        }else {
+            //找到的值
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(mid);
+            //找到值，但是继续向左右扫描
+            //向左扫描
+            int temp1 = mid - 1;
+            while (true){
+                //到最左边没找到则退出
+                if (temp1 < left || arr[temp1] != target)
+            }
+        }
     }
 }
